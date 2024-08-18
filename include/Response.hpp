b9255b6e887b25fef0fbd257a6644f0b4c85c507
mleibeng <mleibeng@student.42.fr>
2024-08-18 05:25:33 +0200

    some additional considerations and examples of GET Requests, HTTP communication  as comments

--- include/Response.hpp
+++ include/Response.hpp
@@ -6,9 +6,27 @@
 /*   By: mleibeng <mleibeng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 02:43:06 by mleibeng          #+#    #+#             */
-/*   Updated: 2024/08/18 02:45:18 by mleibeng         ###   ########.fr       */
+/*   Updated: 2024/08/18 05:08:10 by mleibeng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// 	  Most HTTP communication consists of a retrieval request (GET) for a
+//    representation of some resource identified by a URI.  In the simplest
+//    case, this might be accomplished via a single bidirectional
+//    connection (===) between the user agent (UA) and the origin
+//    server (O).
+
+//             request   >
+//        UA ======================================= O
+//                                    <   response
+
+//  A "gateway" (a.k.a. "reverse proxy") is an intermediary that acts as
+//    an origin server for the outbound connection but translates received
+//    requests and forwards them inbound to another server or servers.
+//    Gateways are often used to encapsulate legacy or untrusted
+//    information services, to improve server performance through
+//    "accelerator" caching, and to enable partitioning or load balancing
+//    of HTTP services across multiple machines.
 
 #ifndef RESPONSE_HPP
 #define RESPONSE_HPP
